ebe99fa9ef93fd8a033d8ee87fd0633a1f7743f6
wlandau-lilly <will.landau@lilly.com>
2020-07-30 18:23:23 -0400

    Update slides

--- docs/_targets.R
+++ docs/_targets.R
@@ -5,7 +5,4 @@ tar_options(packages = c("keras", "tidyverse", "rsample", "recipes",
     "yardstick"))
 tar_pipeline(tar_target(churn_file, "data/customer_churn.csv", 
     format = "file"), tar_target(churn_data, split_data(churn_file)), 
-    tar_target(churn_recipe, prepare_recipe(churn_data)), tar_target(run_relu, 
-        test_model(act1 = "relu", churn_data, churn_recipe)), 
-    tar_target(run_sigmoid, test_model(act1 = "sigmoid", churn_data, 
-        churn_recipe)))
+    tar_target(churn_recipe, prepare_recipe(churn_data)))
