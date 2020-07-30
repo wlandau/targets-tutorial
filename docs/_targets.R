ecfcbb32073780ba0edc84464cd4dd926c822bfe
wlandau-lilly <will.landau@lilly.com>
2020-07-30 18:20:16 -0400

    Add images

--- docs/_targets.R
+++ docs/_targets.R
@@ -8,8 +8,4 @@ tar_pipeline(tar_target(churn_file, "data/customer_churn.csv",
     tar_target(churn_recipe, prepare_recipe(churn_data)), tar_target(run_relu, 
         test_model(act1 = "relu", churn_data, churn_recipe)), 
     tar_target(run_sigmoid, test_model(act1 = "sigmoid", churn_data, 
-        churn_recipe)), tar_target(run_softmax, test_model(act1 = "softmax", 
-        churn_data, churn_recipe)), tar_target(best_run, bind_rows(run_relu, 
-        run_sigmoid, run_softmax) %>% top_n(1, accuracy) %>% 
-        head(1)), tar_target(best_model, train_best_model(best_run, 
-        churn_recipe), format = "keras"))
+        churn_recipe)))
