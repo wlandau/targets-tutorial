776d05dd763c5785351738203ab725e2abd14cc3
wlandau-lilly <will.landau@lilly.com>
2020-07-30 18:25:18 -0400

    Update slides

--- docs/_targets.R
+++
@@ -1,8 +0,0 @@
-library(targets)
-library(targets)
-source("R/functions.R")
-tar_options(packages = c("keras", "tidyverse", "rsample", "recipes", 
-    "yardstick"))
-tar_pipeline(tar_target(churn_file, "data/customer_churn.csv", 
-    format = "file"), tar_target(churn_data, split_data(churn_file)), 
-    tar_target(churn_recipe, prepare_recipe(churn_data)))
