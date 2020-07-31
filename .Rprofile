dd6ade847cdaf5c68a13efc2d16b92b5d014ba1e
wlandau <will.landau@gmail.com>
2020-07-31 15:09:02 -0400

    Condense config

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,12 @@
 source("renv/activate.R")
+options(
+  crayon.enabled = FALSE,
+  readr.show_progress = FALSE,
+  tidyverse.quiet = TRUE,
+  warnPartialMatchArgs = FALSE
+)
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  paged.print = FALSE
+)
--- cloud.R
+++ cloud.R
@@ -1,7 +1,10 @@
 # RStudio Cloud setup
 renv::restore()
-line <- paste0("WORKON_HOME=", file.path(getwd(), "virtualenvs"))
-writeLines(line, ".Renviron")
+lines <- c(
+  "TAR_SCRIPT_ASK=false",
+  paste0("WORKON_HOME=", file.path(getwd(), "virtualenvs"))
+)
+writeLines(lines, ".Renviron")
 rstudioapi::restartSession()
 reticulate::install_miniconda("miniconda")
 reticulate::virtualenv_create("r-reticulate", python = "miniconda/bin/python")
