febeedae5deefaf42339f52f44cf11d7b96da51c
wlandau <will.landau@gmail.com>
2020-08-04 23:11:33 -0400

    Rework setup

--- setup.R
+++ setup.R
@@ -1,16 +1,6 @@
+install.packages("reticulate")
+try(reticulate::install_miniconda("miniconda"))
+renv::use_python("miniconda/bin/python")
 renv::restore()
-lines <- c(
-  "TAR_SCRIPT_ASK=false",
-  paste0("WORKON_HOME=", file.path(getwd(), "virtualenvs"))
-)
-writeLines(lines, ".Renviron")
-rstudioapi::restartSession()
-reticulate::install_miniconda("miniconda")
-reticulate::virtualenv_create("r-reticulate", python = "miniconda/bin/python")
-keras::install_keras(
-  method = "virtualenv",
-  conda = "miniconda/bin/conda",
-  envname = "r-reticulate",
-  restart_session = FALSE
-)
+writeLines("TAR_SCRIPT_ASK=false", ".Renviron")
 rstudioapi::restartSession()
