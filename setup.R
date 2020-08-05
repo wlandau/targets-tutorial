bcb23d3a32189b06a5a30c5ba4d8e46356fdaf9e
wlandau <will.landau@gmail.com>
2020-08-05 00:56:29 -0400

    Ignore virtualenvs

--- setup.R
+++ setup.R
@@ -1,5 +1,19 @@
-install.packages("reticulate")
-try(reticulate::install_miniconda("miniconda"))
-renv::use_python("miniconda/bin/python")
 renv::restore()
-writeLines("TAR_SCRIPT_ASK=false", ".Renviron")
+lines <- c(
+  "TAR_SCRIPT_ASK=false",
+  paste0("RETICULATE_CONDA=", file.path(getwd(), "miniconda", "bin", "conda")),
+  paste0("RETICULATE_PYTHON=", file.path(getwd(), "miniconda", "bin", "python")),
+  paste0("WORKON_HOME=", file.path(getwd(), "virtualenvs"))
+)
+writeLines(lines, ".Renviron")
+rstudioapi::restartSession()
+reticulate::install_miniconda("miniconda")
+reticulate::virtualenv_create(
+  envname = "r-tensorflow",
+  python = Sys.getenv("RETICULATE_PYTHON")
+)
+keras::install_keras(
+  method = "virtualenv",
+  conda = Sys.getenv("RETICULATE_CONDA"),
+  envname = "r-tensorflow"
+)
