4554d6def41222702d8c2c3b334d1a6f11d5229e
wlandau <will.landau@gmail.com>
2020-08-08 13:13:51 -0400

    Add next _targets.R file

---
+++ 5-branching/iteration_targets.R
@@ -0,0 +1,24 @@
+library(targets)
+source("5-branching/functions.R")
+tar_option_set(
+  packages = c(
+    "corrr",
+    "keras",
+    "recipes",
+    "rsample",
+    "tidyverse",
+    "yardstick"
+  )
+)
+tar_pipeline(
+  tar_target(churn_file, "data/churn.csv", format = "file"), 
+  tar_target(churn_data, split_data(churn_file)),
+  tar_target(churn_recipe,  prepare_recipe(churn_data)),
+  tar_target(units, c(16, 32, 64)),
+  tar_target(activations, c("relu", "sigmoid", "softmax")),
+  tar_target(
+    run,
+    test_model(act1 = activations, units1 = units, churn_data, churn_recipe),
+    pattern = cross(activations, units)
+  )
+)
