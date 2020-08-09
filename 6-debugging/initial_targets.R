f277d2c725bdb228cbdfb396ba55f35e76cb4e14
wlandau <will.landau@gmail.com>
2020-08-08 22:24:37 -0400

    Add debugging chapter

---
+++ 6-debugging/initial_targets.R
@@ -0,0 +1,35 @@
+library(targets)
+source("6-debugging/functions.R")
+tar_option_set(
+  packages = c(
+    "corrr",
+    "keras",
+    "recipes",
+    "rsample",
+    "tidyverse",
+    "yardstick"
+  )
+)
+tar_pipeline(
+  tar_target(churn_file, "data/churn.csv", format = "file"), 
+  tar_target(churn_data, split_data(churn_file)),
+  tar_target(churn_recipe,  prepare_recipe(churn_data)),
+  tar_target(units, c(16, 32, 64)),
+  tar_target(activations, c("relu", "sigmoid", "softmax")),
+  tar_target(
+    run,
+    test_model(act1 = activations, units1 = units, churn_data, churn_recipe),
+    pattern = cross(activations, units)
+  ),
+  tar_target(
+    best_run,
+    run %>%
+      top_n(1, accuracy) %>%
+      head(1)
+  ),
+  tar_target(
+    best_model,
+    retrain_run(best_run, churn_recipe),
+    format = "keras"
+  )
+)
