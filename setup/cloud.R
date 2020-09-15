b72e98aa4e79ccffc52771010fdc6bcf0e00ec31
Will Landau <will.landau@lilly.com>
2020-09-15 08:17:14 -0400

    Set tf env var

--- setup/cloud.R
+++ setup/cloud.R
@@ -19,6 +19,7 @@ root <- rprojroot::find_rstudio_root_file()
 reticulate::install_miniconda(file.path(root, "miniconda"))
 lines <- c(
   "TAR_SCRIPT_ASK=false",
+  "TF_CPP_MIN_LOG_LEVEL=1",
   paste0("CONDA=", file.path(root, "miniconda", "bin", "conda")),
   paste0("WORKON_HOME=", file.path(root, "virtualenvs")),
   paste0("RETICULATE_PYTHON=", file.path(root, "miniconda", "bin", "python"))
