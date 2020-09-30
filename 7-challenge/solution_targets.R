159f0b5449ff8acdd6ba1f316e1424401eb39fc5
wlandau <will.landau@gmail.com>
2020-09-30 00:38:11 -0400

    Add an alterantive solution

--- 7-challenge/solution_targets.R
+++ 7-challenge/solution_targets.R
@@ -100,7 +100,7 @@
 # It goes in the _targets.R file in your working directory.
 library(targets)
 library(tarchetypes)
-source("7-review/functions.R")
+source("7-challenge/functions.R")
 tar_option_set(
   packages = c(
     "corrr",
@@ -143,6 +143,53 @@ tar_pipeline(
   tar_target(
     best_run,
     bind_rows(run_relu, run_softmax, run_sigmoid) %>%
+      top_n(1, accuracy) %>%
+      head(1)
+  )
+)
+
+# Here is an equivalent solution using static branching functions
+# tar_map() and tar_combine() from the tarchetypes package.
+library(targets)
+library(tarchetypes)
+source("7-challenge/functions.R")
+tar_option_set(
+  packages = c(
+    "corrr",
+    "keras",
+    "recipes",
+    "rsample",
+    "tidyverse",
+    "yardstick"
+  )
+)
+runs <- tar_map(
+  values = list(act1 = c("relu", "softmax", "sigmoid")),
+  tar_target(
+    run,
+    test_model(act1 = act1, churn_data, churn_recipe),
+    pattern = map(churn_data, churn_recipe)
+  )
+)
+tar_pipeline(
+  tar_files(churn_file, list.files("data", full.names = TRUE)),
+  tar_target(
+    churn_data,
+    split_data(churn_file),
+    pattern = map(churn_file),
+    iteration = "list"
+  ),
+  tar_target(
+    churn_recipe,
+    prepare_recipe(churn_data),
+    pattern = map(churn_data),
+    iteration = "list"
+  ),
+  runs,
+  tar_combine(
+    best_run,
+    runs,
+    command = bind_rows(!!!.x) %>%
       top_n(1, accuracy) %>%
       head(1)
   )
